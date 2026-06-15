6eda573fea7d2e0b15cfc0bf7dffd5d7dd372931
Dharmendra Prajapati <dharmendra97095@gmail.com>
2026-06-15 13:03:32 +0530

    all post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ import MapSection from "../components/MapSection";
 import ProjectSlider from "../components/ProjectSlider";
 import OurProgramsSection from "../components/OurProgramsSection";
 import BeforeAfterImpact from "../components/BeforeAfterImpact";
-import PartnersOpportunities from "../components/PartnersOpportunities";
 
 const PROGRAMS_API_URL = `${API_BASE_URL}/programs.php?t=` + Date.now();
 const SUBSCRIBE_API_URL = `${API_BASE_URL}/subscribe.php`;
@@ -467,8 +466,6 @@ const Home = () => {
           </div>
         </div>
       </section>
-
-      <PartnersOpportunities/>
 
       <BeforeAfterImpact />
 
